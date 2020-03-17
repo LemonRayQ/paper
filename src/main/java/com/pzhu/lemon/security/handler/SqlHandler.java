e4109144b51fbd52ecd1a821142bb959499030fd
LemonRayQ <1291498269@qq.com>
2020-03-17 20:57:01 +0800

    'test'

---
+++ src/main/java/com/pzhu/lemon/security/handler/SqlHandler.java
@@ -0,0 +1,13 @@
+package com.pzhu.lemon.security.handler;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: paper
+ * @description: sql注入处理器
+ * @author: LemonQ
+ * @create: 2020-03-17 20:56
+ **/
+@Component
+public class SqlHandler {
+}
